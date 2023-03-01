4a36718ff3d812f836ca91c4d628adb89e905d16
Kevin Labianco <klabianco@gmail.com>
2023-03-01 15:46:41 -0800

    returning the first description

--- src/YoutubeAPI.php
+++ src/YoutubeAPI.php
@@ -2,7 +2,7 @@
 
 class YoutubeAPI
 {
-    public $_query, $_channelId, $_resultsPerPage, $_key, $_order, $_type, $_pageToken;
+    public $_query, $_channelId, $_resultsPerPage, $_key, $_order, $_type, $_pageToken, $_videoId, $_method;
 
     public function __construct($key = '', $query = '', $channelId = '', $resultsPerPage = '')
     {
@@ -10,14 +10,22 @@ class YoutubeAPI
         $this->_query = $query;
         $this->_channelId = $channelId;
         $this->_resultsPerPage = $resultsPerPage;
+        $this->_method = "search";
     }
 
     public function getYoutubeJson()
     {
-        $url = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=" . $this->_channelId . "&maxResults=" . $this->_resultsPerPage . "&key=" . $this->_key
+        $url = "https://youtube.googleapis.com/youtube/v3/".$this->_method."?key=".$this->_key."&part=snippet";
+        
+        if($this->_method == "search"){
+        $url .= "&channelId=" . $this->_channelId . "&maxResults=" . $this->_resultsPerPage . "&key=" . $this->_key
             . "&order=viewCount"
             . "&type=video";
+        }
 
+        if($this->_videoId != ''){
+            $url .= "&id=" . $this->_videoId;
+        }
         if ($this->_query != "") $url .= "&q=" . $this->_query;
         if ($this->_pageToken != '') $url .= "&pageToken=" . $this->_pageToken;
 
--- www/index.php
+++ www/index.php
@@ -1,12 +1,18 @@
 <?php
 require __DIR__ . '/../../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../..");
+$dotenv->load();
+
 require_once __DIR__ . '/../src/AI.php';
 require_once __DIR__ . '/../src/YoutubeAPI.php';
 
 $AI = new AI();
-$Youtube = new YoutubeAPI();
+$Youtube = new YoutubeAPI($_ENV['YOUTUBE_API_KEY']);
+$Youtube->_videoId = "nmu4FE79y_8";
+$Youtube->_method = "videos";
+$json = $Youtube->getYoutubeJson();
 
-$output = "gm";
+$details = json_decode($json);
 
-echo json_encode($output);
+$description = $details->items[0]->snippet->description;
